b1ab7b09e14d9c10c2925cf1f11e5698b43b0d98
Tarek <talatrach@gmail.com>
2023-02-23 23:11:58 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-function App() {
+import Header from './components/Layout/Header';
+
+function App () {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
